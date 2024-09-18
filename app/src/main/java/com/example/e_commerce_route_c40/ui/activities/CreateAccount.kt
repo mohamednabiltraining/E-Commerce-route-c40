ef0f6ae5031b9c5f6743ab24c9134aeec97ddb9b
mazensherif007 <135878735+mazensherif007@users.noreply.github.com>
2024-09-18 21:41:34 +0300

    todo

--- app/src/main/java/com/example/e_commerce_route_c40/ui/activities/CreateAccount.kt
+++ app/src/main/java/com/example/e_commerce_route_c40/ui/activities/CreateAccount.kt
@@ -2,7 +2,6 @@ package com.example.e_commerce_route_c40.ui.activities
 
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
 import androidx.fragment.app.viewModels
 import com.example.e_commerce_route_c40.R
 import com.example.e_commerce_route_c40.base.BaseFragment
